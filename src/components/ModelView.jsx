f0b7d29407d117ba53e9fc0433e481f6404f1634
ZANGETSU\khanh <khanhduywin1907@gmail.com>
2024-09-21 13:51:59 +0700

    checkpoint 1

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ModelView = () => {
+	return (
+		<div className="border-red-200">
+			Can't understand the model D:, need to know Thrre Js frfr
+		</div>
+	);
+};
+
+export default ModelView;
